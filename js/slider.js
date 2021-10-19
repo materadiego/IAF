5a80983f9163d6adc3703f05e8e068a6ee17cbe2
Diego Matera <diegomatera@gmail.com>
2021-10-19 14:15:39 -0300

    seccion instalaciones y paginas terminadas

--- js/slider.js
+++ js/slider.js
@@ -2,6 +2,10 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
